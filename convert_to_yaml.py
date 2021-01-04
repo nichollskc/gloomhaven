ae2997815014086ad0a98b565b17198de253b427
Kath Nicholls <nichollskc@gmail.com>
2021-01-04 17:36:50 +0000

    Refactor to take all information from yml file

--- convert_to_yaml.py
+++ convert_to_yaml.py
@@ -52,18 +52,18 @@ def convert_completion_dict_yaml():
     }
 
     all_attempts = {}
+    scenario_to_completion_date = {}
     for scenario_num, attempt_info in completion_dict.items():
         print(attempt_info)
         attempts = re.findall(r'COLOR="#[\w]{6}">(\w+) (\d+)/(\d+)/(\d\d)', attempt_info)
-        loot_left = re.findall(r'COLOR="#[\w]{6}">([\w ]+ left)', attempt_info)
+        loot_left = re.search(r'COLOR="#[\w]{6}">([\w ]+ left)', attempt_info)
+        print(loot_left)
 
         for attempt in attempts:
             print(attempt)
             attempt_dict = {
                 "scenario": int(scenario_num),
             }
-            if attempt[0] != "Completed":
-                attempt_dict["failure"] = True
             day = attempt[1]
             if len(day) == 1:
                 day = "0" + day
@@ -72,12 +72,112 @@ def convert_completion_dict_yaml():
                 month = "0" + month
             year = "20" + attempt[3]
             attempt_date = f"{year}/{month}/{day}"
+
+            if attempt[0] == "Completed":
+                scenario_to_completion_date[scenario_num] = attempt_date
+            else:
+                attempt_dict["failure"] = True
+
             all_attempts[attempt_date] = attempt_dict
+
+        if loot_left:
+            attempt_dict["loot_left"] = loot_left[1]
+
 
     print(all_attempts)
 
-    return all_attempts
+    return all_attempts, scenario_to_completion_date
 
-ATTEMPTS = convert_completion_dict_yaml()
+def construct_yaml():
+    edges = [
+        (1, 2, "Continue", True),
+        (1, 68, "Chest loot", False, True),
+        (2, 3, "Help Jekserah"),
+        (2, 4, "Investigate Gloom"),
+        (3, 82, "Road event", False, True),
+        (3, 8, "Help Jekserah"),
+        (3, 9, "Spy on Jekserah"),
+        (8, 7, "Find Jekserah"),
+        (8, 13, "Ask enchanter for help"),
+        (8, 14, "Ask enchanter for help"),
+        (8, 7, "Spy on Jekserah"),
+        (14, 7, "Help from Hail"),
+        (14, 19, "Help from Hail"),
+        (14, 31, "Help from Hail"),
+        (14, 43, "Help from Hail"),
+        (43, 26, "Use water-breathing"),
+        (43, 37, "Use water-breathing"),
+        (7, 20, "Fight Jekserah"),
+        (20, 28, "Jekserah's warning"),
+        (20, 16, "Help Captain"),
+        (20, 18, "Help Captain"),
+        (16, 24, "Follow the Voice"),
+        (16, 25, "Continue ascent", True),
+        (24, 64, "Chest loot", False, True),
+        (24, 32, "Help the Voice"),
+        (24, 30, "Investigate the Voice"),
+        (25, 34, "Fight the Drake", True),
+        (25, 33, "Help the Drake"),
+        (4, 5, "Disrupt cult"),
+        (4, 6, "Help cult"),
+        (5, 10, "Enter the void", True),
+        (5, 14, "Close the void"),
+        (5, 19, "Close the void"),
+        (10, 21, "Fight the Demon", True),
+        (10, 22, "Find artifact"),
+        (32, 61, "Pyper's Personal Quest"),
+        (32, 33, "Find vessel"),
+        (32, 40, "Find vessel"),
+        (40, 41, "Continue", True),
+        (37, 47, "Continue", True),
+        (37, 17, "Chest loot", False, True),
+        (47, 51, "End corruption"),
+        (17, 71, "Chest loot", False, True),
+        (13, 15, "Gain power"),
+        (22, 35, "Fight for evil"),
+        (22, 36, "Fight against evil"),
+        (22, 31, "Artifact to Hail"),
+        (31, 38, "Find evil"),
+        (31, 39, "Find evil"),
+        (38, 44, "Help Orchid village"),
+        (38, 48, "Find evil in forest"),
+        (48, 51, "End corruption"),
+        (61, 74, "City event", False, True),
+        (61, 62, "Lighthouse basement", True),
+        (62, 72, "Town records", False, True),
+    ]
+
+    attempts_dict, scenario_to_completion_date = convert_completion_dict_yaml()
+    for edge in edges:
+        from_id = str(edge[0])
+        to_id = str(edge[1])
+        description = edge[2]
+        linked = False
+        unusual = False
+        try:
+            linked = edge[3]
+        except IndexError:
+            pass
+        try:
+            unusual = edge[4]
+        except IndexError:
+            pass
+
+        attempt = scenario_to_completion_date[from_id]
+        link = {"description": description}
+        if unusual:
+            link["unusual"] = True
+        if linked:
+            link["linked"] = True
+
+        attempt_info = attempts_dict[attempt]
+        if "links" in attempt_info:
+            attempt_info["links"][to_id] = link
+        else:
+            attempt_info["links"] = {to_id: link}
+
+    return {"attempts": attempts_dict}
+
+ATTEMPTS = construct_yaml()
 with open("tmp/scenario_info.yml", "w") as f:
     yaml.dump(ATTEMPTS, f)
--- scenario_dag.py
+++ scenario_dag.py
@@ -6,36 +6,8 @@ import yaml
 from graphviz import Digraph
 
 def build_labels():
-    scenario_list = "1.G-10Black Barrow 2.G-11Barrow Lair 3.G-3Inox Encampment 4.E-11Crypt of the " \
-                    "Damned 5.D-6Ruinous Crypt 6.F-10Decaying Crypt 7.C-12Vibrant Grotto " \
-                    "8.C-18Gloomhaven Warehouse 9.L-2Diamond Mine 10.C-7Plane of Elemental Power " \
-                    "11.B-16Gloomhaven Square A 12.B-16Gloomhaven Square B 13.N-3Temple of the " \
-                    "Seer 14.C-10Frozen Hollow 15.B-11Shrine of Strength 16.B-6Mountain Pass " \
-                    "17.K-17Lost Island 18.C-14Abandoned Sewers 19.M-7Forgotten Crypt " \
-                    "20.H-13Necromancer’s Sanctum 21.C-7Infernal Throne 22.K-8Temple of the " \
-                    "Elements 23.C-15Deep Ruins 24.C-6Echo Chamber 25.A-5Icecrag Ascent " \
-                    "26.D-15Ancient Cistern 27.E-6Ruinous Rift 28.E-4Outer Ritual Chamber " \
-                    "29.E-3Sanctuary of Gloom 30.N-15Shrine of the Depths 31.A-16Plane of Night " \
-                    "32.L-11Decrepit Wood 49.N-7Rebel’s Stand 50.C-17Ghost Fortress 51.A-15The " \
-                    "Void 52.D-14Noxious Cellar 53.F-11Crypt Basement 54.D-8Palace of Ice " \
-                    "55.G-5Foggy Thicket 56.G-4Bandits Wood 57.D-14Investigation 58.E-15Bloody " \
-                    "Shack 59.F-1Forgotten Grove 60.B-15Alchemy Lab 61.N-11Fading Lighthouse " \
-                    "62.O-11Pit of Souls 63.M-1Magma Pit 64.K-16Underwater Lagoon 65.L-5Sulfur " \
-                    "Mine 66.G-14Clockwork Cove 67.K-2Arcane Library 68.N-8Toxic Moor 69.F-8Well " \
-                    "of the Unfortunate 70.J-17Chained Isle 71.K-5Windswept Highlands " \
-                    "72.H-12Oozing Grove 73.N-5Rockslide Ridge 74.I-14Merchant Ship " \
-                    "75.G-12Overgrown Graveyard 76.L-3Harrower Hive 77.B-17Vault of Secrets " \
-                    "78.B-14Sacrifice Pit 79.K-12Lost Temple 80.K-1Vigil Keep 33.A-7Savvas Armory " \
-                    "34.A-4Scorched Summit 35.A-14Gloomhaven Battlements A 36.B-14Gloomhaven " \
-                    "Battlements B 37.G-18Doom Trench 38.G-2Slave Pens 39.B-11Treacherous Divide " \
-                    "40.F-12Ancient Defense Network 41.F-13Timeworn Tomb 42.C-5Realm of the Voice " \
-                    "43.D-4Drake Nest 44.F-3Tribal Assault 45.M-9Rebel Swamp 46.A-11Nightmare " \
-                    "Peak 47.H-18Lair of the Unseeing Eye 48.E-1Shadow Weald 81.D-2Temple of the " \
-                    "Eclipse 82.M-6Burning Mountain 83.C-15Shadows Within 84.D-12Crystalline Cave " \
-                    "85.M-3Sun Temple 86.D-15Harried Village 87.I-9Corrupted Cove 88.D-16Plane of " \
-                    "Water 89.C-17Syndicate Hideout 90.J-7Demonic Rift 91.E-2Wild Melee " \
-                    "92.C-14Back Alley Brawl 93.N-17Sunken Vessel 94.F-12Vermling Nest " \
-                    "95.G-12Payment Due "
+    with open("scenario_list.txt", "r") as f:
+        scenario_list = f.read()
 
     extracted_coordinates = re.findall(r'(\d+).([A-Z]\-\d+)', scenario_list)
     extracted_names = re.findall(r'(\d+).[A-Z]\-\d\d?([A-Za-z ]+)', scenario_list)
@@ -50,6 +22,21 @@ def add_edge(graph, first_node, second_node, explanation, linked=False, unusual=
     else:
         graph.edge(str(first_node), str(second_node), label=f"    {explanation}   ")
 
+def format_line(text, color, style):
+    return f"<TR><TD><{style}><FONT COLOR=\"{color}\">{text}</FONT></{style}></TD></TR>"
+
+def format_attempt(attempt_info, date):
+    if attempt_info.get("failure", False):
+        main_line = format_line(f"Attempted {date}", "#A9A9A9", "b")
+    else:
+        main_line = format_line(f"Completed {date}", "#BF111D", "b")
+
+    if "loot_left" in attempt_info:
+        loot_line = format_line(attempt_info["loot_left"], "#BF111D", "i")
+    else:
+        loot_line = ""
+
+    return main_line + loot_line
 
 def build_graph():
     dot = Digraph(comment = 'Gloomhaven Scenarios')
@@ -58,74 +45,51 @@ def build_graph():
     with open("scenario_info.yml", "r") as f:
         scenario_info = yaml.load(f, Loader=yaml.FullLoader)
 
-    scenario_blocks = scenario_info["scenario_clusters"]
-    other_scenarios = [15, 17, 64, 68, 71, 72, 74, 82]
+    print(scenario_info["attempts"])
+    all_open_scenarios = set()
+    all_labelled_scenarios = set()
+    scenario_labels = {}
+    for date, attempt in scenario_info["attempts"].items():
+        scenario_num = str(attempt["scenario"])
 
-    scenarios = other_scenarios + list(itertools.chain.from_iterable([d['scenarios'] for d in scenario_blocks.values()]))
+        try:
+            existing_label = scenario_labels[scenario_num]
+        except KeyError:
+            existing_label = ""
+
+        label = existing_label + format_attempt(attempt, date)
+
+        scenario_labels[scenario_num] = label
+
+        all_labelled_scenarios.add(scenario_num)
+        all_open_scenarios.add(scenario_num)
+        try:
+            for linked in attempt["links"].keys():
+                all_open_scenarios.add(linked)
+        except:
+            pass
+
+    for unlabelled in all_open_scenarios.difference(all_labelled_scenarios):
+        scenario_labels[unlabelled] = ""
+
+    scenario_blocks = scenario_info["scenario_clusters"]
+    included_in_cluster = set()
 
     scenario_names, coordinate_labels = build_labels()
-
-    completion_dict = dict([(str(i), '') for i in scenarios])
-    completion_dict.update({
-        '1': '<TR><TD><b><FONT COLOR="#BF111D">Completed 13/2/19</FONT></b></TD></TR>',
-        '2': '<TR><TD><b><FONT COLOR="#BF111D">Completed 14/2/19</FONT></b></TD></TR>'
-             '<TR><TD><i><FONT COLOR="#BF111D">Side rooms left</FONT></i></TD></TR>',
-        '3': '<TR><TD><b><FONT COLOR="#A9A9A9">Attempted 15/2/19</FONT></b></TD></TR>'
-             '<TR><TD><b><FONT COLOR="#BF111D">Completed 16/2/19</FONT></b></TD></TR>'
-             '<TR><TD><i><FONT COLOR="#BF111D">Side rooms left</FONT></i></TD></TR>',
-        '8': '<TR><TD><b><FONT COLOR="#BF111D">Completed 17/2/19</FONT></b></TD></TR>'
-             '<TR><TD><i><FONT COLOR="#BF111D">Chest left</FONT></i></TD></TR>',
-        '82': '<TR><TD><b><FONT COLOR="#BF111D">Completed 19/2/19</FONT></b></TD></TR>',
-        '14': '<TR><TD><b><FONT COLOR="#BF111D">Completed 2/3/19</FONT></b></TD></TR>',
-        '43': '<TR><TD><b><FONT COLOR="#BF111D">Completed 9/3/19</FONT></b></TD></TR>'
-              '<TR><TD><i><FONT COLOR="#BF111D">Side rooms left</FONT></i></TD></TR>',
-        '7': '<TR><TD><b><FONT COLOR="#BF111D">Completed 23/3/19</FONT></b></TD></TR>',
-        '20': '<TR><TD><b><FONT COLOR="#BF111D">Completed 7/4/19</FONT></b></TD></TR>',
-        '16': '<TR><TD><b><FONT COLOR="#BF111D">Completed 25/5/19</FONT></b></TD></TR>',
-        '24': '<TR><TD><b><FONT COLOR="#A9A9A9">Attempted 9/6/19</FONT></b></TD></TR>'
-              '<TR><TD><b><FONT COLOR="#BF111D">Completed 23/6/19</FONT></b></TD></TR>',
-        '25': '<TR><TD><b><FONT COLOR="#BF111D">Completed 27/6/19</FONT></b></TD></TR>',
-        '34': '<TR><TD><b><FONT COLOR="#BF111D">Completed 29/6/19</FONT></b></TD></TR>',
-        '4': '<TR><TD><b><FONT COLOR="#BF111D">Completed 30/6/19</FONT></b></TD></TR>',
-        '5': '<TR><TD><b><FONT COLOR="#BF111D">Completed 7/7/19</FONT></b></TD></TR>',
-        '10': '<TR><TD><b><FONT COLOR="#BF111D">Completed 14/7/19</FONT></b></TD></TR>',
-        '21': '<TR><TD><b><FONT COLOR="#BF111D">Completed 31/7/19</FONT></b></TD></TR>',
-        '68': '<TR><TD><b><FONT COLOR="#BF111D">Completed 18/9/19</FONT></b></TD></TR>',
-        '32': '<TR><TD><b><FONT COLOR="#BF111D">Completed 21/9/19</FONT></b></TD></TR>',
-        '33': '<TR><TD><b><FONT COLOR="#A9A9A9">Attempted 30/9/19</FONT></b></TD></TR>'
-              '<TR><TD><b><FONT COLOR="#BF111D">Completed 6/10/19</FONT></b></TD></TR>',
-        '40': '<TR><TD><b><FONT COLOR="#BF111D">Completed 9/11/19</FONT></b></TD></TR>',
-        '41': '<TR><TD><b><FONT COLOR="#BF111D">Completed 16/11/19</FONT></b></TD></TR>',
-        '37': '<TR><TD><b><FONT COLOR="#BF111D">Completed 14/12/19</FONT></b></TD></TR>',
-        '47': '<TR><TD><b><FONT COLOR="#BF111D">Completed 11/01/20</FONT></b></TD></TR>'
-              '<TR><TD><i><FONT COLOR="#BF111D">Chest left</FONT></i></TD></TR>',
-        '17': '<TR><TD><b><FONT COLOR="#BF111D">Completed 12/01/20</FONT></b></TD></TR>',
-        '13': '<TR><TD><b><FONT COLOR="#BF111D">Completed 16/01/20</FONT></b></TD></TR>',
-        '15': '<TR><TD><b><FONT COLOR="#A9A9A9">Attempted 01/02/20</FONT></b></TD></TR>',
-        '22': '<TR><TD><b><FONT COLOR="#BF111D">Completed 15/02/20</FONT></b></TD></TR>',
-        '31': '<TR><TD><b><FONT COLOR="#BF111D">Completed 16/02/20</FONT></b></TD></TR>',
-        '38': '<TR><TD><b><FONT COLOR="#A9A9A9">Attempted 23/02/20</FONT></b></TD></TR>'
-              '<TR><TD><b><FONT COLOR="#BF111D">Completed 01/03/20</FONT></b></TD></TR>',
-        '44': '<TR><TD><b><FONT COLOR="#BF111D">Completed 08/03/20</FONT></b></TD></TR>',
-        '48': '<TR><TD><b><FONT COLOR="#A9A9A9">Attempted 14/03/20</FONT></b></TD></TR>'
-              '<TR><TD><b><FONT COLOR="#BF111D">Completed 21/03/20</FONT></b></TD></TR>',
-        '39': '<TR><TD><b><FONT COLOR="#A9A9A9">Attempted 22/11/20</FONT></b></TD></TR>',
-        '64': '<TR><TD><b><FONT COLOR="#BF111D">Completed 20/12/20</FONT></b></TD></TR>',
-        '61': '<TR><TD><b><FONT COLOR="#BF111D">Completed 24/12/20</FONT></b></TD></TR>',
-        '62': '<TR><TD><b><FONT COLOR="#BF111D">Completed 26/12/20</FONT></b></TD></TR>',
-    })
 
     for name, block in scenario_blocks.items():
         with dot.subgraph(name=f"cluster_{name}") as cluster:
             cluster.attr(label=block['description'], fontsize='40')
             for i in block['scenarios']:
+                included_in_cluster.add(i)
+
                 image_file = f"locations/location_{i}.png"
                 assert os.path.isfile(image_file), f"Expected image file {image_file}"
                 label = f"""<<TABLE BORDER="0">
                             <TR><TD><IMG SCALE="TRUE" SRC="{image_file}"/></TD></TR>
                             <TR><TD><b>{scenario_names[str(i)]}</b></TD></TR>
                             <TR><TD><b>{coordinate_labels[str(i)]}</b></TD></TR>
-                            {completion_dict[str(i)]}
+                            {scenario_labels[str(i)]}
                             </TABLE>>"""
                 print(label)
                 cluster.node(name=str(i),
@@ -134,14 +98,14 @@ def build_graph():
                          fontsize='30',
                          penwidth='0')
 
-    for i in other_scenarios:
+    for i in all_open_scenarios.difference(included_in_cluster):
         image_file = f"locations/location_{i}.png"
         assert os.path.isfile(image_file), f"Expected image file {image_file}"
         label = f"""<<TABLE BORDER="0">
                     <TR><TD><IMG SCALE="TRUE" SRC="{image_file}"/></TD></TR>
                     <TR><TD><b>{scenario_names[str(i)]}</b></TD></TR>
                     <TR><TD><b>{coordinate_labels[str(i)]}</b></TD></TR>
-                    {completion_dict[str(i)]}
+                    {scenario_labels[str(i)]}
                     </TABLE>>"""
         print(label)
         dot.node(name=str(i),
@@ -156,65 +120,16 @@ def build_graph():
                      'weight': '10',
                      'fontname':'times'}
 
-    # dot.edge('1', '1to2', arrowhead='none')
-    # dot.node(name='1to2', penwidth='0', label='Continue through the lair')
-    # dot.edge('1to2', '2')
 
-    add_edge(dot, 1, 2, "Continue", linked=True)
-    add_edge(dot, 1, 68, "Chest loot", unusual=True)
-    add_edge(dot, 2, 3, "Help Jekserah")
-    add_edge(dot, 2, 4, "Investigate Gloom")
-    add_edge(dot, 3, 82, "Road event", unusual=True)
-    add_edge(dot, 3, 8, "Help Jekserah")
-    add_edge(dot, 3, 9, "Spy on Jekserah")
-    add_edge(dot, 8, 7, "Find Jekserah")
-    add_edge(dot, 8, 13, "Ask enchanter for help")
-    add_edge(dot, 8, 14, "Ask enchanter for help")
-    add_edge(dot, 8, 7, "Spy on Jekserah")
-    add_edge(dot, 14, 7, "Help from Hail")
-    add_edge(dot, 14, 19, "Help from Hail")
-    add_edge(dot, 14, 31, "Help from Hail")
-    add_edge(dot, 14, 43, "Help from Hail")
-    add_edge(dot, 43, 26, "Use water-breathing")
-    add_edge(dot, 43, 37, "Use water-breathing")
-    add_edge(dot, 7, 20, "Fight Jekserah")
-    add_edge(dot, 20, 28, "Jekserah's warning")
-    add_edge(dot, 20, 16, "Help Captain")
-    add_edge(dot, 20, 18, "Help Captain")
-    add_edge(dot, 16, 24, "Follow the Voice")
-    add_edge(dot, 16, 25, "Continue ascent", linked=True)
-    add_edge(dot, 24, 64, "Chest loot", unusual=True)
-    add_edge(dot, 24, 32, "Help the Voice")
-    add_edge(dot, 24, 30, "Investigate the Voice")
-    add_edge(dot, 25, 34, "Fight the Drake", linked=True)
-    add_edge(dot, 25, 33, "Help the Drake")
-    add_edge(dot, 4, 5, "Disrupt cult")
-    add_edge(dot, 4, 6, "Help cult")
-    add_edge(dot, 5, 10, "Enter the void", linked=True)
-    add_edge(dot, 5, 14, "Close the void")
-    add_edge(dot, 5, 19, "Close the void")
-    add_edge(dot, 10, 21, "Fight the Demon", linked=True)
-    add_edge(dot, 10, 22, "Find artifact")
-    add_edge(dot, 32, 61, "Pyper's Personal Quest")
-    add_edge(dot, 32, 33, "Find vessel")
-    add_edge(dot, 32, 40, "Find vessel")
-    add_edge(dot, 40, 41, "Continue", linked=True)
-    add_edge(dot, 37, 47, "Continue", linked=True)
-    add_edge(dot, 37, 17, "Chest loot", unusual=True)
-    add_edge(dot, 47, 51, "End corruption")
-    add_edge(dot, 17, 71, "Chest loot", unusual=True)
-    add_edge(dot, 13, 15, "Gain power")
-    add_edge(dot, 22, 35, "Fight for evil")
-    add_edge(dot, 22, 36, "Fight against evil")
-    add_edge(dot, 22, 31, "Artifact to Hail")
-    add_edge(dot, 31, 38, "Find evil")
-    add_edge(dot, 31, 39, "Find evil")
-    add_edge(dot, 38, 44, "Help Orchid village")
-    add_edge(dot, 38, 48, "Find evil in forest")
-    add_edge(dot, 48, 51, "End corruption")
-    add_edge(dot, 61, 74, "City event", unusual=True)
-    add_edge(dot, 61, 62, "Lighthouse basement", linked=True)
-    add_edge(dot, 62, 72, "Town records", unusual=True)
+    for attempt_date, attempt in scenario_info["attempts"].items():
+        links = attempt.get("links", {})
+        for link, link_info in links.items():
+            add_edge(dot,
+                     int(attempt["scenario"]),
+                     int(link),
+                     link_info["description"],
+                     linked=link_info.get("linked", False),
+                     unusual=link_info.get("unusual", False))
 
     dot.render('Gloomhaven_scenario_route.gv', view=True)
 
